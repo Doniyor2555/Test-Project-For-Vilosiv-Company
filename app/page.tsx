26ad9f1a7a2ed9f393455b23fcbedee1a8ba3b90
DoniyorShodiyevDoni <109288985+Doniyor2555@users.noreply.github.com>
2024-01-08 20:00:54 +0500

    Test-Project-For-Vilosiv-Company

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import CardList from "./components/cardList";
 import CustomDropDown from "./components/customDropdown";
 import Header from "./components/header";
-import DeleteModal from "./components/modals/deleteModal";
+
 import getTodos from "./hooks/getTodos";
 
 export default async function Home({
@@ -10,30 +10,30 @@ export default async function Home({
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
   const todos = await getTodos();
-  const params = searchParams["search"] ?? "";
+  // @ts-ignore
+  const params:string = searchParams["search"] ?? "";
 
   const filterByStatus: any = searchParams["filterByStatus"] ?? "";
 
-  const filterBySearchValue =
-    params === "search"
-      ? todos.filter((item) =>
-          item?.title.toLowerCase()?.includes(params.toLowerCase())
-        )
-      : filterByStatus === "Done"
-      ? todos.filter((item) =>
-          item.status.toLocaleLowerCase().includes(filterByStatus.toLowerCase())
-        )
-      : filterByStatus === "In Progress"
-      ? todos.filter((item) =>
-          item.status.toLocaleLowerCase().includes(filterByStatus.toLowerCase())
-        )
-      : filterByStatus === "Waiting for execution"
-      ? todos.filter((item) =>
-          item.status.toLocaleLowerCase().includes(filterByStatus.toLowerCase())
-        )
-      : filterByStatus === "All"
-      ? todos.filter((item) => item)
-      : todos.filter((item) => item);
+  const filterBySearchValue = params
+    ? todos.filter((item) =>
+        item.title?.toLocaleLowerCase()?.includes(params.toLowerCase())
+      )
+    : filterByStatus === "Done"
+    ? todos.filter((item) =>
+        item.status.toLocaleLowerCase().includes(filterByStatus.toLowerCase())
+      )
+    : filterByStatus === "In Progress"
+    ? todos.filter((item) =>
+        item.status.toLocaleLowerCase().includes(filterByStatus.toLowerCase())
+      )
+    : filterByStatus === "Waiting for execution"
+    ? todos.filter((item) =>
+        item.status.toLocaleLowerCase().includes(filterByStatus.toLowerCase())
+      )
+    : filterByStatus === "All"
+    ? todos.filter((item) => item)
+    : todos.filter((item) => item);
 
   return (
     <div>
